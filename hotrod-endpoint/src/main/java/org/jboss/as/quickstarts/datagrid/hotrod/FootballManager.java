ae7587599148a9b4ccef27d79053a78e7a8538f0
Red F. Mulder <red.fox.mulder@gmail.com>
2013-07-12 16:32:23 -0400

    Initial

---
+++ hotrod-endpoint/src/main/java/org/jboss/as/quickstarts/datagrid/hotrod/FootballManager.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+package org.jboss.as.quickstarts.datagrid.hotrod;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+
+/**
+ * @author Martin Gencur
+ */
+public class FootballManager {
+
+    private static final String JDG_HOST = "jdg.host";
+    private static final String HOTROD_PORT = "jdg.hotrod.port";
+    private static final String PROPERTIES_FILE = "jdg.properties";
+    private static final String msgTeamMissing = "The specified team \"%s\" does not exist, choose next operation\n";
+    private static final String msgEnterTeamName = "Enter team name: ";
+    private static final String initialPrompt = "Choose action:\n" + "============= \n" + "at  -  add a team\n"
+            + "ap  -  add a player to a team\n" + "rt  -  remove a team\n" + "rp  -  remove a player from a team\n"
+            + "p   -  print all teams and players\n" + "q   -  quit\n" + "ar  -  add referees\n" + "pr  -  print referees\n";
+    private static final String teamsKey = "teams";
+    private static final String cacheName = "default";
+
+    private Console con;
+    private RemoteCacheManager cacheManager;
+    private RemoteCache<String, Object> cache;
+
+    public FootballManager(Console con) {
+        this.con = con;
+        cacheManager = new RemoteCacheManager(jdgProperty(JDG_HOST) + ":" + jdgProperty(HOTROD_PORT));
+        cache = cacheManager.getCache(cacheName);
+        if(!cache.containsKey(teamsKey)) {
+            List<String> teams = new ArrayList<String>();
+            Team t = new Team("Barcelona");
+            t.addPlayer("Messi");
+            t.addPlayer("Pedro");
+            t.addPlayer("Puyol");
+            cache.put(t.getName(), t);
+            teams.add(t.getName());
+            cache.put(teamsKey, teams);
+        }
+    }
+
+    public void addTeam() {
+        String teamName = con.readLine(msgEnterTeamName);
+        @SuppressWarnings("unchecked")
+        List<String> teams = (List<String>) cache.get(teamsKey);
+        if (teams == null) {
+            teams = new ArrayList<String>();
+        }
+        Team t = new Team(teamName);
+        cache.put(teamName, t);
+        teams.add(teamName);
+        // maintain a list of teams under common key
+        cache.put(teamsKey, teams);
+    }
+
+    public void addPlayers() {
+        String teamName = con.readLine(msgEnterTeamName);
+        String playerName = null;
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            while (!(playerName = con.readLine("Enter player's name (to stop adding, type \"q\"): ")).equals("q")) {
+                t.addPlayer(playerName);
+            }
+            cache.put(teamName, t);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void removePlayer() {
+        String playerName = con.readLine("Enter player's name: ");
+        String teamName = con.readLine("Enter player's team: ");
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            t.removePlayer(playerName);
+            cache.put(teamName, t);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void removeTeam() {
+        String teamName = con.readLine(msgEnterTeamName);
+        Team t = (Team) cache.get(teamName);
+        if (t != null) {
+            cache.remove(teamName);
+            @SuppressWarnings("unchecked")
+            List<String> teams = (List<String>) cache.get(teamsKey);
+            if (teams != null) {
+                teams.remove(teamName);
+            }
+            cache.put(teamsKey, teams);
+        } else {
+            con.printf(msgTeamMissing, teamName);
+        }
+    }
+
+    public void printTeams() {
+        @SuppressWarnings("unchecked")
+        List<String> teams = (List<String>) cache.get(teamsKey);
+        if (teams != null) {
+            for (String teamName : teams) {
+                con.printf(cache.get(teamName).toString());
+            }
+        }
+    }
+    
+    private static final String refereeKeyPrefix = "referee";
+    private static final int numOfReferees = 100000;
+    private static final int printEvery = 10000;
+    public void addReferees() {
+        for(int i = 0; i < numOfReferees; i++) {
+            cache.put(refereeKeyPrefix + i, refereeKeyPrefix + i);
+            if (i % printEvery == 0) {
+                con.printf("Records inserted %1$d \n", i);
+            }
+        }
+    }
+    
+    public void printReferees() {
+        long min = 0, max = 0;
+        for(int i = 0; i < numOfReferees; i++) {
+            long start = System.currentTimeMillis();
+            //con.printf(cache.get(refereeKeyPrefix + i).toString());
+            String obj = cache.get(refereeKeyPrefix + i).toString();
+            if (obj == null) {
+                con.printf("--->> Key not found: %1$d \n", i);
+            }
+            long end = System.currentTimeMillis();
+            if (end - start < min) {
+                min = end - start;
+            }
+            if (end - start > max) {
+                max = end - start;
+            }
+            if (i % printEvery == 0) {
+                con.printf("Records fetched %1$d \n", i);
+            }
+            //con.printf("\n");
+        }
+        con.printf("Fetch min=%1$d max=%2$d \n", min, max);
+    }
+
+    public void stop() {
+        cacheManager.stop();
+    }
+
+    public static void main(String[] args) {
+        Console con = System.console();
+        FootballManager manager = new FootballManager(System.console());
+        con.printf(initialPrompt);
+
+        while (true) {
+            String action = con.readLine(">");
+            if ("at".equals(action)) {
+                manager.addTeam();
+            } else if ("ap".equals(action)) {
+                manager.addPlayers();
+            } else if ("rt".equals(action)) {
+                manager.removeTeam();
+            } else if ("rp".equals(action)) {
+                manager.removePlayer();
+            } else if ("p".equals(action)) {
+                manager.printTeams();
+            } else if ("q".equals(action)) {
+                manager.stop();
+                break;
+            } else if ("ar".equals(action)) {
+                manager.addReferees();
+            } else if ("pr".equals(action)) {
+                manager.printReferees();
+            }
+        }
+    }
+
+    public static String jdgProperty(String name) {
+        Properties props = new Properties();
+        try {
+            props.load(FootballManager.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE));
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        return props.getProperty(name);
+    }
+}
